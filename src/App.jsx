0e19f5be0547d8468721d40e883229e7b3eef7a0
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-09-12 00:25:10 -0700

    created fake api,loaded data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import Blogs from './component/Blogs/Blogs'
+import Header from './component/Header/Header'
 
 function App() {
  
   return (
     <>
-     
+    <Header></Header>
+    <Blogs></Blogs>
     </>
   )
 }
